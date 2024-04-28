eb1d46552f0aaded288f67ec6011f5b3a7c90b4e
Filipe Carvalho <fcf0133@gmail.com>
2024-04-28 11:59:24 -0300

    Criação dos objetos

--- Singleton/src/Main.java
+++ Singleton/src/Main.java
@@ -1,5 +1,32 @@
+import Fabrica.FabricaDeFiguras;
+import Figuras.Circulo;
+import Figuras.Quadrado;
+import Figuras.Triangulo;
+import excecoes.*;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ValorMenorOuIgualAZeroException {
+        Circulo circulo;
+        Triangulo trianguloRetangulo;
+        Triangulo trianguloEquilatero;
+        Triangulo trianguloEscaleno;
+        Quadrado quadrado1;
+        Quadrado quadrado2;
+        Quadrado quadrado3;
+        Quadrado quadrado4;
+        Quadrado quadrado5;
+
+        Circulo circulo2;
+
+        circulo = FabricaDeFiguras.getCirculo(6);
+        circulo2 = FabricaDeFiguras.getCirculo(9);
+
+        System.out.println("\n" + circulo);
+        System.out.printf("Área: %.2f\n", circulo.calcularArea());
+        System.out.printf("Perímetro: %.2f\n", circulo.calcularPerimetro());
+
+        System.out.println("\n" + circulo2);
+        System.out.printf("Área: %.2f\n", circulo2.calcularArea());
+        System.out.printf("Perímetro: %.2f\n", circulo2.calcularPerimetro());
 
     }
 }
